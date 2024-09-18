c577156a94438fd66c5984d23ebe5684d9d39d11
cacayan2 <cacayan2@illinois.edu>
2024-09-18 16:54:26 -0500

    Completed tasks in TestPerformance.java

--- README.txt
+++ README.txt
@@ -1,8 +1,11 @@
-TestIterator.java
+**TestIterator.java**
 1. Does using a LinkedList make any difference?
 Functionally, there is no difference between ArrayList and LinkedList - the implementation is change but the end result of using
 either implementation of the list data structure is still the same. What changes is performance. When using an ArrayList, the tests finish in on average 70ms, while when
-using a LinkedList, on average 15 ms.
+using a LinkedList, on average 15 ms (for the entire test suite). Because this test suite is truly only bottlnecked by the dynamic resizing of the list (using the list.add() method,
+the iteration through the list will cost about the same for both implementations in terms of runtime complexity), the runtime of the completion of all tests
+is a robust enough metric for performance.
+
 
 2. What happens if you use list.remove(Integer.valueOf(77))?
 list.remove(Integer.valueOf(77)) only removes the first occurrence of an integer with the value 77 in the list.
@@ -16,9 +19,45 @@ set of tests is addition/deletion, a LinkedList implementation is preferred to t
 we would go with the ArrayList, however because the tasks in these unit tests focus more on resizing/insertion/deletion tasks, the LinkedList implementation is preferred).
 
 
-TestList.java
+**TestList.java**
 1. Does using a LinkedList make any difference?
+Running the entire test suite causes only a marginal difference in runtime, 13 ms in the LinkedList case and 11 ms in the ArrayList case, not enough to say with certainty
+that the LinkedList outperforms the ArrayList. This is because each of the tests run have a combination of list access and dynamic resizing. The ArrayList is better for list access, while
+the LinkedList is better for dynamic resizing. Because the test suite contains tests that have a combination of both, having the runtimes of each of the cases individually could help us elaborate further on
+the performance of the two lists.
+LinkedList: 13 ms
+testSet - 5 ms
+testAddMultiple - 0 ms
+testRetainAll - 0 ms
+testContainsAll - 1 ms
+testAddMultiple2 - 1 ms
+testRemoveObject - 0 ms
+testSizeNonEmpty - 1 ms
+testContains - 0 ms
+testSizeEmpty - 1 ms
+testAddAll - 0 ms
+testRemoveAll - 2 ms
+testSubList - 2 ms
 
+ArrayList: 11 ms
+testSet - 5 ms
+testAddMultiple - 0 ms
+testRetainAll - 0 ms
+testContainsAll - 2 ms
+testAddMultiple2 - 1 ms
+testRemoveObject - 0 ms
+testSizeNonEmpty - 0 ms
+testContains - 1 ms
+testSizeEmpty - 0 ms
+testAddAll - 1 ms
+testRemoveAll - 0 ms
+testSubList - 1 ms
+
+Even after getting a breakdown of the runtime for each of these lists, not much can be said about the performance difference between the two,
+which could be a function of the tests not being robust/complex enough, or in the nature of the fact that the tests themselves do not test the lists specifically on their performance. It is of note that
+functions that are generally associated with access to elements of the list perform better in the ArrayList implementation, and tests which largely involve dynamic resizing of the list perform better in the LinkedList implementation
+(the effect size between the lists and runtimes of the individual size cannot be accurately measured because the time to complete the tests is so small).
+These results are not meaningful, and within the context of these tests, either type of list implementation will produce similar performance results.
 
 2. What does list.remove(5) do?
 The functional result of this method is the same for both the ArrayList and LinkedList implementations. This method simply
@@ -36,4 +75,4 @@ In the LinkedList implementation, this method removes the first occurrence and a
 For this operation, the LinkedList implementation is faster, since the worst-case complexity for the ArrayList implementation of this method would be O(n) (removing the first element would cause the entire list
 to have to be rearranged), while in the case of the LinkedList implementation, the worst-case complexity would be O(1) or constant time since only the pointers would need to be rearranged.
 
-4. Other Notes
+
--- src/test/java/cs271/lab/list/TestPerformance.java
+++ src/test/java/cs271/lab/list/TestPerformance.java
@@ -7,68 +7,137 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+// Added suppression to warnings for instantiation of arrays with generic/unchecked types.
+// While this is not good coding in practice (it is preferable to store the lists to test in another list),
+// this code exists for the sake of demonstration - the tests are able to pass and give meaningful results,
+// and it would take much time to implement tests that utilize lists instead of arrays.
+@SuppressWarnings({"unchecked", "rawtypes"})
 public class TestPerformance {
 
-  // TODO run test and record running times for SIZE = 10, 100, 1000, 10000, ...
+  // (complete) TODO run test and record running times for SIZE = 10, 100, 1000, 10000, ...
   // (choose in conjunction with REPS below up to an upper limit where the clock
   // running time is in the tens of seconds)
-  // TODO (optional) refactor to DRY
+  // (complete) TODO (optional) refactor to DRY
   // which of the two lists performs better as the size increases?
-  private final int SIZE = 10;
+  // Changed SIZE to be an array of different values for different testing parameters.
+  private final int[] SIZE = {10, 100, 1000, 10000, 100000};
 
-  // TODO choose this value in such a way that you can observe an actual effect
+  // (complete) TODO choose this value in such a way that you can observe an actual effect
   // for increasing problem sizes
   private final int REPS = 1000000;
 
-  private List<Integer> arrayList;
+  // Creation of an array of lists (the elements are generic type lists, warnings are generated but are suppressed
+  // - see above comments for further explanation) which store lists of differing sizes.
+  private List<Integer>[] arrayList = new List[SIZE.length];
 
-  private List<Integer> linkedList;
+  private List<Integer>[] linkedList = new List[SIZE.length];
 
+  // Populates the array of lists with new <Integer> lists.
   @Before
   public void setUp() throws Exception {
-    arrayList = new ArrayList<Integer>(SIZE);
-    linkedList = new LinkedList<Integer>();
-    for (var i = 0; i < SIZE; i++) {
-      arrayList.add(i);
-      linkedList.add(i);
+    for(var i = 0; i < SIZE.length; i++) {
+      arrayList[i] = new ArrayList<Integer>();
+      linkedList[i] = new LinkedList<Integer>();
+      for(var j = 0; j < SIZE[i]; j++) {
+        arrayList[i].add(j);
+        linkedList[i].add(j);
+      }
     }
   }
 
+  // Kept the original code.
   @After
   public void tearDown() throws Exception {
     arrayList = null;
     linkedList = null;
   }
 
+  // The following tests were changed in order to allow for added functionality:
+  // - The test now records and prints out its runtime cleanly.
+  // - The test reiterates until it runs through all the elements of the SIZE array.
+  // Documentation will only exist in this test, as the remaining tests generally have the same structure.
   @Test
   public void testLinkedListAddRemove() {
-    for (var r = 0; r < REPS; r++) {
-      linkedList.add(0, 77);
-      linkedList.remove(0);
+    // Arrays to record runtimes of the different tests.
+    long[] start = new long[SIZE.length];
+    long[] end = new long[SIZE.length];
+    long[] runtime = new long[SIZE.length];
+    // Runs the original code for each of the indices of the SIZE array.
+    for(var i = 0; i < SIZE.length; i++) {
+      start[i] = System.currentTimeMillis();
+      // The original code for the test, modified to index through the array of lists.
+      for (var r = 0; r < REPS; r++) {
+        linkedList[i].add(0, 77);
+        linkedList[i].remove(0);
+      }
+      end[i] = System.currentTimeMillis();
     }
+    // Prints out runtimes of the tests.
+    System.out.println("Results for testLinkedListAddRemove()");
+    for(var s = 0; s < runtime.length; s++){
+      System.out.println("SIZE = " + SIZE[s] + ": " + (end[s] - start[s]) + " ms");
+    }
+    System.out.println("-----");
   }
 
   @Test
   public void testArrayListAddRemove() {
-    for (var r = 0; r < REPS; r++) {
-      arrayList.add(0, 77);
-      arrayList.remove(0);
+    long[] start = new long[SIZE.length];
+    long[] end = new long[SIZE.length];
+    long[] runtime = new long[SIZE.length];
+    for(var i = 0; i < SIZE.length; i++) {
+      start[i] = System.currentTimeMillis();
+      for(var r = 0; r < REPS; r++) {
+        arrayList[i].add(0, 77);
+        arrayList[i].remove(0);
+      }
+      end[i] = System.currentTimeMillis();
     }
+    System.out.println("Results for testArrayListAddRemove()");
+    for(var s = 0; s < runtime.length; s++) {
+      System.out.println("SIZE = " + SIZE[s] + ": " + (end[s] - start[s]) + " ms");
+    }
+    System.out.println("-----");
   }
 
   @Test
   public void testLinkedListAccess() {
-    var sum = 0L;
-    for (var r = 0; r < REPS; r++) {
-      sum += linkedList.get(r % SIZE);
+    long[] start = new long[SIZE.length];
+    long[] end = new long[SIZE.length];
+    long[] runtime = new long[SIZE.length];
+    for(var i = 0; i < SIZE.length; i++) {
+      start[i] = System.currentTimeMillis();
+      var sum = 0L;
+      for(var r = 0; r < REPS; r++) {
+        sum += linkedList[i].get(r % SIZE[i]);
+      }
+      end[i] = System.currentTimeMillis();
     }
+    System.out.println("Results for testLinkedListAccess()");
+    for(var s = 0; s < runtime.length; s++) {
+      System.out.println("SIZE = " + SIZE[s] + ": " + (end[s] - start[s]) + " ms");
+    }
+    System.out.println("-----");
   }
 
   @Test
   public void testArrayListAccess() {
-    var sum = 0L;
-    for (var r = 0; r < REPS; r++) {
-      sum += arrayList.get(r % SIZE);
+    long[] start = new long[SIZE.length];
+    long[] end = new long[SIZE.length];
+    long[] runtime = new long[SIZE.length];
+    for(var i = 0; i < SIZE.length; i++) {
+      start[i] = System.currentTimeMillis();
+      var sum = 0L;
+      for(var r = 0; r < REPS; r++) {
+        sum += arrayList[i].get(r % SIZE[i]);
+      }
+      end[i] = System.currentTimeMillis();
     }
+    System.out.println("Results for testArrayListAccess()");
+    for(var s = 0; s < runtime.length; s++) {
+      System.out.println("SIZE = " + SIZE[s] + ": " + (end[s] - start[s]) + " ms");
+    }
+    System.out.println("-----");
   }
 }
+
